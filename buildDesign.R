14b56961d09ac838ceec4759f21306b5a1d0e962
HideoFu <h.fushimi.x689@gmail.com>
2017-07-11 18:13:27 +0900

    Update buildDesign.R

--- buildDesign.R
+++ buildDesign.R
@@ -1,8 +1,8 @@
-# y はDGEList オブジェクト
+# y is a DGEList object
 
 buildDesign <- function(y){
   design <- model.matrix(~0+group, data=y$samples)
   colnames(design) <- levels(y$samples$group)
-  
+
   invisible(design)
 }
