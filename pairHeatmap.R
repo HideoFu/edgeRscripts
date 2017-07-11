ffd93173c882a1970de58aadb2a4f1972b4771ad
HideoFu <h.fushimi.x689@gmail.com>
2017-07-11 18:30:55 +0900

    Update pairHeatmap.R

--- pairHeatmap.R
+++ pairHeatmap.R
@@ -1,11 +1,11 @@
-#QLF ‚ğæ‚Éì¬
+# make a heatmap of most significantly changed n genes.
 
 pairHeatmap <- function(y, qlf, cexR=0.8, n=50){
 
 	library(gplots)
 	library(statmod)
 
-	#fit ‚Ìì¬
+	# make fit object
 
 	top <- topTags(qlf, n=n)
 
@@ -19,6 +19,6 @@ pairHeatmap <- function(y, qlf, cexR=0.8, n=50){
 	datatable2 <- cpm(y)[m,]
 	data <- log2(as.matrix(datatable2)+1)
 
-        heatmap.2(data, hclustfun=myclust, distfun=mydist, na.rm = TRUE, scale="none", dendrogram="both", margins=c(6,7), Rowv=TRUE, Colv=TRUE, symbreaks=FALSE, key=TRUE, symkey=FALSE, density.info="none", trace="none", main="log CPM", cexRow=cexR, cexCol=1, labRow=datatable[,"symbol"],col=rev(heat.colors(75)))
+  heatmap.2(data, hclustfun=myclust, distfun=mydist, na.rm = TRUE, scale="none", dendrogram="both", margins=c(6,7), Rowv=TRUE, Colv=TRUE, symbreaks=FALSE, key=TRUE, symkey=FALSE, density.info="none", trace="none", main="log CPM", cexRow=cexR, cexCol=1, labRow=datatable[,"symbol"],col=rev(heat.colors(75)))
 
 }
