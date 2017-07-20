16f5ccf354a64a0e9d68da12d33d94f8b0512021
HideoFu <h.fushimi.x689@gmail.com>
2017-07-20 10:28:33 +0900

    Change return object "str"  from string to vector

--- anovContrast.R
+++ anovContrast.R
@@ -3,12 +3,13 @@ anovContrast <- function(design){
 	group <- attr(design,"dimnames")[[2]]
 	k <- length(group)
 
-	str <- as.character()
+	str <- character()
+	str0 <- as.character()
 
 	for (i in 1:(k-1)){
 		for (j in (i+1):k){
-		str0 <- paste0(group[i],"vs",group[j]," = ",group[i]," - ",group[j], ", ")
-		str <- paste0(str, str0)
+		str0 <- paste0(group[i],"vs",group[j]," = ",group[i]," - ",group[j])
+		str <- c(str,str0)
 		}
 	}
 
